3d4dc56f434a88f864d36e0ed33f643d81a1bb96
Baovy1106 <phamthibaovy202011@gmail.com>
2024-11-22 19:10:09 +0700

    Ps42404_PhamThiBaoVy_Lab5

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import bai1 from './components/bai1.vue'
+import bai2 from './components/bai2.vue'
+import bai3 from './components/bai3.vue'
+
+import { ref } from 'vue'
+import CreatePost from './components/CreatePost.vue'
+import PostList from './components/PostList.vue'
+
+const posts = ref([])
+
+function addPost(post) {
+posts.value.push(post)
+}
+</script>
+
+<template>
+    <div id="app">
+        <h1>Ứng dụng Blog với VueJS</h1>
+        <br>
+        <CreatePost @add-post="addPost"></CreatePost>
+        <br>
+        <PostList :posts="posts"></PostList>
+    </div>
+</template>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
